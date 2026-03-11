4403af2e837ea65d0de8d75e4239da6e97aade56
itsadhin <itsadhin@gmail.com>
2026-03-11 23:40:57 +0530

    Modified readfile.py

--- readfile.py
+++ readfile.py
@@ -2,7 +2,39 @@ import requests
 
 url = "https://huggingface.co/datasets/itsmeG/Birds_denmark/resolve/main/Database/occurrence.txt"
 
-# Stream line by line and does NOT save the full file locally
+columns_needed = [
+    "species",
+    "genus",
+    "decimalLatitude",
+    "decimalLongitude",
+    "eventDate"
+]
+
+# Stream through each line withour downloading locally
 with requests.get(url, stream=True) as r:
-    for line in r.iter_lines():
-        print(line.decode())
+    # iter_lines makes sure only one line is read at a time,
+    # decode_unicode=True converts bytes from server to python string
+    rows = r.iter_lines(decode_unicode=True)
+    header = next(rows).split("\t")    # gets the first line of dataset that is coloumn name
+    
+    indices = []        # used to store index of required columns
+    for col in columns_needed:
+        if col in header:
+            idx = header.index(col)
+            indices.append(idx)
+    print("\t".join(columns_needed)) # adds tab space between each column
+
+    for line in rows:
+        row = line.split("\t")
+        
+        # Filtering rows with blanks
+        try:
+            filtered = []
+
+            for i in indices:
+                filtered.append(row[i])
+            print("\t".join(filtered))
+        except IndexError: 
+            continue
+
+
