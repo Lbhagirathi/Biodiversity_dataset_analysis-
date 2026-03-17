86c8fb17ada2f063baf6d30429b622388ab0041d
itsadhin <itsadhin@gmail.com>
2026-03-17 16:14:04 +0530

    Added simple dashreader

---
+++ dash/dashplot.py
@@ -0,0 +1,14 @@
+import dash_ag_grid as dag
+from dash import Dash
+import pandas as pd
+
+app = Dash()
+
+df = pd.read_csv("../trial_data/birds.csv").iloc[:,1:] # this is just a sample contaning ~300K rows
+
+app.layout = dag.AgGrid(
+    rowData=df.to_dict("records"),
+    columnDefs=[{"field": i} for i in df.columns],
+)
+
+app.run(debug=True)
