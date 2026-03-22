93bbf71bc58872757184c97c5eca412b1b22b4d7
itsadhin <itsadhin@gmail.com>
2026-03-22 17:27:52 +0530

    Modified dashplot.py

--- dash/dashplot.py
+++ dash/dashplot.py
@@ -1,14 +1,22 @@
 import dash_ag_grid as dag
-from dash import Dash
+from dash import Dash, dcc, html   
 import pandas as pd
+import plotly.express as px
 
 app = Dash()
 
-df = pd.read_csv("./birds.csv").iloc[:,1:] # this is just a sample contaning ~300K rows
+df = pd.read_csv("./birds.csv").iloc[:,1:]
 
-app.layout = dag.AgGrid(
-    rowData=df.to_dict("records"),
-    columnDefs=[{"field": i} for i in df.columns],
-)
+birds = {}
+for species in df["species"]:
+    if species not in birds:
+        birds[species] = 1
+    else:
+        birds[species] += 1
+
+x = list(birds.keys())
+y = list(birds.values())
+
+app.layout = html.Div([dcc.Graph(figure=px.bar(x=x, y=y,labels={"x": "Species","y":"Count"} ))])
 
 app.run(debug=True)
