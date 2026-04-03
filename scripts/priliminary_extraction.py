2786fdc0e01be27ba6833830cfe243a7251da730
Lbhagirathi <l.bhagirathirao@students.iiserpune.ac.in>
2026-04-03 14:55:00 +0530

    organised

--- scripts/priliminary_extraction.py
+++ scripts/priliminary_extraction.py
@@ -82,106 +82,6 @@ def identify_hotspots(summary):
 
 
 # =========================
-# 6. MAPS
-# =========================
-def plot_maps(summary, hotspots):
-
-    print("📍 Generating biodiversity map...")
-
-    gdf = gpd.GeoDataFrame(
-        summary,
-        geometry=gpd.points_from_xy(summary["lon_bin"], summary["lat_bin"]),
-        crs="EPSG:4326"
-    )
-
-    denmark = gpd.read_file("denmark_grid.gpkg")
-    denmark = denmark.to_crs("EPSG:4326")
-
-    print("Total grid cells:", len(gdf))
-
-    fig, ax = plt.subplots(figsize=(10,10))
-
-    # =========================
-    # OCEAN BACKGROUND
-    # =========================
-    ax.set_facecolor("#b7dff5")   # light blue ocean
-
-    # =========================
-    # DENMARK LAND
-    # =========================
-    denmark.plot(
-        ax=ax,
-        color="#f0f0f0",
-        edgecolor="black",
-        linewidth=0.8,
-        zorder=1
-    )
-
-    # =========================
-    # ALL GRID CELLS (LAND + SEA)
-    # =========================
-    gdf.plot(
-        ax=ax,
-        column="richness",
-        cmap="viridis",
-        markersize=100,
-        legend=True,
-        legend_kwds={
-            "label": "Species Richness",
-            "shrink": 0.6
-        },
-        alpha=0.9,
-        edgecolor="black",
-        linewidth=0.2,
-        zorder=2
-    )
-
-    # =========================
-    # HOTSPOTS
-    # =========================
-    hotspot_gdf = gdf[
-        gdf["grid_cell_id"].isin(hotspots["grid_cell_id"])
-    ]
-
-    if len(hotspot_gdf) > 0:
-        hotspot_gdf.plot(
-            ax=ax,
-            color="red",
-            markersize=200,
-            edgecolor="black",
-            linewidth=0.5,
-            label="Hotspots",
-            zorder=3
-        )
-
-    # =========================
-    # MAP SETTINGS
-    # =========================
-    ax.set_xlim(7,13)
-    ax.set_ylim(54,58)
-
-    ax.set_title(
-        "Biodiversity Hotspots in Denmark",
-        fontsize=16,
-        fontweight="bold"
-    )
-
-    ax.set_axis_off()
-
-    ax.legend(loc="lower left")
-
-    plt.savefig(
-        "results/plots/biodiversity_hotspots.png",
-        dpi=600,
-        bbox_inches="tight"
-    )
-
-    plt.show()
-
-    print("✅ Map saved!")
-
-
-# =========================
 # 7. STATS + IUCN
 # =========================
 def compute_stats(df, summary, occupancy, restricted):
@@ -255,8 +155,6 @@ def main():
     summary = combine_metrics(df, richness)
 
     hotspots = identify_hotspots(summary)
-
-    plot_maps(summary, hotspots)
 
     compute_stats(df, summary, occupancy, restricted)
 
